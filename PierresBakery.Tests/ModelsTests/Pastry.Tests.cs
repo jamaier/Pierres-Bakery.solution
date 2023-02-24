ac1f39823b11a91072e7aa65555450e74d8f6a9c
Jacob Maier <jacobamaier@gmail.com>
2023-02-24 11:46:41 -0800

    Pastry Test #1: PASS

--- PierresBakery.Tests/ModelsTests/Pastry.Tests.cs
+++ PierresBakery.Tests/ModelsTests/Pastry.Tests.cs
@@ -10,8 +10,18 @@ namespace PierresBakery.ModelsTests
         [TestMethod]
     public void Bread_CreateInstanceOfBread_Bread()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(0);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+    [TestMethod]
+    public void Bread_GetQuantityAndCostOfBread_Bread()
+    {
+      Pastry newPastry = new Pastry(1);
+      int price = 2;
+      int quantity = 1;
+      Assert.AreEqual(price, newPastry.Price);
+      Assert.AreEqual(quantity, newPastry.Quantity);
     }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -3,6 +3,12 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+    public Pastry(int quantity)
+    {
+      Price = 2;
+      Quantity = quantity;
+    }
   }
 }
